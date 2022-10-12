0e2fee192774adfb7dbbd4e4a10f0beb01942673
KanishkaNaik <kanishkabc123@gmail.com>
2022-10-12 23:30:16 +0530

    63. Install Puppeteer for adding Chrome Headless browser in the karma config file - Jasmine Testing.

--- karma.config.js
+++ karma.config.js
@@ -2,9 +2,10 @@ module.exports = function (config) {
     config.set({
       frameworks: ['jasmine'],
       files: ['./CustomMatcher.js', '*.js', '*.spec.js'],
-      plugins: ['karma-jasmine'],
+      plugins: ['karma-jasmine','karma-chrome-launcher'],
       reporters: ['dots'],
       colors: true,
-      singleRun: false
+      singleRun: false,
+      browsers:['chromeHeadless']
     });
   };
